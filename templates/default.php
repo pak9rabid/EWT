d132436281b69a42930ac251a7e1cb51cbc42874
pgriffin <pak9rabid@yahoo.com>
2014-04-16 21:53:44 -0500

    Removing script link to jquery library

--- templates/default.php
+++ templates/default.php
@@ -13,7 +13,6 @@
 </style>
 
 <!-- javascript -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript">
 <?=$page->javascript()?>
 </script>
